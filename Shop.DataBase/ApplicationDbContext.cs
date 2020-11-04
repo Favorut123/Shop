304dd1b0e2e1f87b5e4ecd758eafd3fd8e8a8e26
Favorut123 <volodarchuhay@gmail.com>
2020-11-04 17:44:25 +0200

    11

---
+++ Shop.DataBase/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Shop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+
+namespace Shop.DataBase
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderProduct> OrderProducts { get; set; }
+
+        //for The entity type 'OrderProduct' requires a primary key to be defined.(migrations)
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<OrderProduct>()
+                .HasKey(x => new { x.ProductId, x.OrderId });        // primary key that consist of TWO OR MORE keys(product and order id`s)
+        }
+
+    }
+}
